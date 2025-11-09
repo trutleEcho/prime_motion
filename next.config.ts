df2674a910c05481755060bf9580bc55439b8091
trutleEcho <trichup20@gmail.com>
2025-11-09 21:11:50 +0530

    v0.0.2 | Fixed issues for vercel deployment.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        domains: ['images.unsplash.com'], // 👈 add this
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
     },
 };
 
